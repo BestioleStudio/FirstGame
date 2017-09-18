d71ab036d2d6e860a2cee1f46f96dc9358531947
Unknown <devil-ced@hotmail.com>
2017-09-18 20:38:48 +0200

    Collision fixed

--- FirstGame/Assets/Scripts/PlayerController.cs
+++ FirstGame/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float moveSpeed;
+	private Rigidbody2D playerRigidbody;
 
 	// Use this for initialization
 	void Start () {
+
+		playerRigidbody = GetComponent<Rigidbody2D>();
 		
 	}
 	
@@ -16,13 +19,26 @@ public class PlayerController : MonoBehaviour {
 
 		if (Input.GetAxisRaw ("Horizontal") > 0.5f || Input.GetAxisRaw ("Horizontal") < -0.5f) 
 		{
-			transform.Translate (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f);
+			//transform.Translate (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f);
+			playerRigidbody.velocity = new Vector2(Input.GetAxisRaw ("Horizontal") * moveSpeed, playerRigidbody.velocity.y);
 		}
 
 		if (Input.GetAxisRaw ("Vertical") > 0.5f || Input.GetAxisRaw ("Vertical") < -0.5f) 
 		{
-			transform.Translate (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f);
+			//transform.Translate (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f);
+			playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, Input.GetAxisRaw ("Vertical") * moveSpeed);
 		}
+
+		if (Input.GetAxisRaw ("Horizontal") < 0.5f && Input.GetAxisRaw ("Horizontal") > -0.5f) 
+		{
+			playerRigidbody.velocity = new Vector2 (0f, playerRigidbody.velocity.y);
+		}
+
+		if (Input.GetAxisRaw ("Vertical") < 0.5f && Input.GetAxisRaw ("Vertical") > -0.5f) 
+		{
+			playerRigidbody.velocity = new Vector2 (playerRigidbody.velocity.x, 0f);
+		}
+
 
 	}
 }
